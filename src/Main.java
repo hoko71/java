a5d9dffa6c9c0be3d03be62336e2bb29564a105f
hoko71 <ketabintrodus@gmail.com>
2025-02-03 14:23:43 +0330

    add java in git first day

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        //declare variables
+        byte x= 33;
+        byte y=22;
+        /*
+        print x , y
+         */
+        System.out.println(x+y);
+    }
+}
